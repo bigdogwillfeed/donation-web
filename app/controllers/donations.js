7b61e0aabb61693b21b4109d3211c20f69c7da29
Eamonn de Leastar <edeleastar@wit.ie>
2016-08-03 15:05:57 +0100

    very simple donation persistence

--- app/controllers/donations.js
+++ app/controllers/donations.js
@@ -12,7 +12,10 @@ exports.home = {
 exports.report = {
 
   handler: function (request, reply) {
-    reply.view('report', { title: 'Donations to Date', });
+    reply.view('report', {
+      title: 'Donations to Date',
+      donations: this.donations,
+    });
   },
 
 };
@@ -20,6 +23,8 @@ exports.report = {
 exports.donate = {
 
   handler: function (request, reply) {
+    const data = request.payload;
+    this.donations.push(data);
     reply.redirect('/report');
   },
 
