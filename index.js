04fe054c6ec93e286ce58ef9556c81a7d983f5c4
Eamonn de Leastar <edeleastar@wit.ie>
2016-08-03 12:25:08 +0100

    revised project structure + first simple biew (using inert)

--- index.js
+++ index.js
@@ -5,12 +5,19 @@ const Hapi = require('hapi');
 var server = new Hapi.Server();
 server.connection({ port: process.env.PORT || 4000 });
 
-server.route(require('./routes'));
+server.register(require('inert'), err => {
 
-server.start(err => {
   if (err) {
     throw err;
   }
 
-  console.log('Server listening at:', server.info.uri);
+  server.route(require('./routes'));
+  server.start((err) => {
+    if (err) {
+      throw err;
+    }
+
+    console.log('Server listening at:', server.info.uri);
+  });
+
 });
