eae51915ad7063083a5950d94cb23b9886b57f40
Eamonn de Leastar <edeleastar@wit.ie>
2016-08-05 12:09:23 +0100

    display donor email in donations

--- app/models/donation.js
+++ app/models/donation.js
@@ -5,6 +5,7 @@ const mongoose = require('mongoose');
 const donationSchema = mongoose.Schema({
   amount: Number,
   method: String,
+  donor: String,
 });
 
 const Donation = mongoose.model('Donation', donationSchema);
